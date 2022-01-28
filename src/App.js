c2f4b352271f6999e7aad0d7bb1c4d4548a81017
azharjustin <azharjustin37@gmail.com>
2022-01-28 15:00:06 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome name="spidy" pic="https://i.insider.com/5f778d302400440019129c6a?width=700"/>
+      <Welcome name="tony"  pic="https://st1.bollywoodlife.com/wp-content/uploads/2019/01/Iron-Man-1.jpg"/>
+      <Welcome name="hulk" pic="https://terrigen-cdn-dev.marvel.com/content/prod/1x/theincrediblehulk_lob_mas_hlf_02_0.jpg"/>
     </div>
   );
 }
+function Welcome({name,pic}) {
+  
+  return (
+    <div>
+      <h1>hello {name}💕</h1>
+      <img className="marvel" src={pic} alt="" />
 
-export default App;
+         </div>  );
+}
